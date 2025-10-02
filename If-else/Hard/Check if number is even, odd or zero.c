12eaad633196406b724709f24f771c920ecbcaee
Chaitrali14mankar <chaitralimankar14@gmail.com>
2025-10-02 23:17:45 +0530

    Create Check if number is even, odd or zero.c

---
+++ If-else/Hard/Check if number is even, odd or zero.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter number: ");
+    scanf("%d",&n);
+    if(n==0)
+        printf("Zero");
+    else if(n%2==0)
+        printf("Even");
+    else
+        printf("Odd");
+    return 0;
+}
