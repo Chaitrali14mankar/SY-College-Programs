7c0cfd47a8b28c174977745377c22d7c4074ace1
Chaitrali14mankar <chaitralimankar14@gmail.com>
2025-10-02 22:18:45 +0530

    Create Find GCD of two numbers.c

---
+++ For Loop/Medium/Find GCD of two numbers.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int a=48, b=18, gcd=1;
+    for(int i=1; i<=a && i<=b; i++) {
+        if(a%i==0 && b%i==0) gcd=i;
+    }
+    printf("GCD = %d", gcd);
+    return 0;
+}
