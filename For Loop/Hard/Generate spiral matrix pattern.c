108d9352738349bbfe3fc79766b2bca7d0aa5967
Chaitrali14mankar <chaitralimankar14@gmail.com>
2025-10-02 22:52:26 +0530

    Create  Generate spiral matrix pattern.c

---
+++ For Loop/Hard/Generate spiral matrix pattern.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    int n=4;
+    int a[4][4];
+    int val=1, top=0, bottom=n-1, left=0, right=n-1;
+    while(top<=bottom && left<=right) {
+        for(int i=left;i<=right;i++) a[top][i]=val++;
+        top++;
+        for(int i=top;i<=bottom;i++) a[i][right]=val++;
+        right--;
+        for(int i=right;i>=left;i--) a[bottom][i]=val++;
+        bottom--;
+        for(int i=bottom;i>=top;i--) a[i][left]=val++;
+        left++;
+    }
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<n;j++) printf("%3d",a[i][j]);
+        printf("\n");
+    }
+    return 0;
+}
