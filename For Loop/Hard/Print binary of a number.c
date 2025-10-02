720bc23d0d61b2cc0fefde7c079ce71cca8e0b92
Chaitrali14mankar <chaitralimankar14@gmail.com>
2025-10-02 22:44:00 +0530

    Update Print binary of a number.c

--- For Loop/Hard/Print binary of a number.c
+++ For Loop/Hard/Print binary of a number.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter number: ");
+    scanf("%d",&n);
+    int bin[32],i=0;
+    for(;n>0;n/=2) bin[i++]=n%2;
+    for(int j=i-1;j>=0;j--) printf("%d",bin[j]);
+    return 0;
+}
 
