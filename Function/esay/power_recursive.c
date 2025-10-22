5dca66b5ba0cdff07de0843f0dfac900f95178c6
Chaitrali14mankar <chaitralimankar14@gmail.com>
2025-10-22 23:31:40 +0530

    Create power_recursive.c

---
+++ Function/esay/power_recursive.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int power(int b,int e){
+    if(e==0) return 1;
+    return b*power(b,e-1);
+}
+int main(){
+    int b,e; scanf("%d %d",&b,&e);
+    printf("%d^%d = %d\n",b,e,power(b,e));
+    return 0;
+}
