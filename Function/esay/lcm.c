14eb8d9bdf51ce4c46de3816565a5003b2ac9d75
Chaitrali14mankar <chaitralimankar14@gmail.com>
2025-10-22 23:27:02 +0530

    Create lcm.c

---
+++ Function/esay/lcm.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int gcd(int a,int b){ return b==0?a:gcd(b,a%b); }
+int lcm(int a,int b){ return (a*b)/gcd(a,b); }
+int main(){
+    int x,y; scanf("%d %d",&x,&y);
+    printf("LCM = %d\n", lcm(x,y));
+    return 0;
+}
