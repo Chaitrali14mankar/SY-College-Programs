ce7d25a19ca1d875a222e3d1c24bc8f0e4772da8
Chaitrali14mankar <chaitralimankar14@gmail.com>
2025-10-22 22:53:26 +0530

    Create sort_descending.c

---
+++ Array/Easy/sort_descending.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int n; printf("n: "); if(scanf("%d",&n)!=1) return 0;
+    int a[100], i, j;
+    for(i=0;i<n;i++) scanf("%d",&a[i]);
+    for(i=0;i<n-1;i++)
+        for(j=0;j<n-1-i;j++)
+            if(a[j]<a[j+1]){ int t=a[j]; a[j]=a[j+1]; a[j+1]=t; }
+    printf("Sorted descending: ");
+    for(i=0;i<n;i++) printf("%d ", a[i]);
+    printf("\n");
+    return 0;
+}
