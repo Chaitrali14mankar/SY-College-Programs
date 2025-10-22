9cfdda20c4bd9edb27d6368a327f56ecab6e9d0f
Chaitrali14mankar <chaitralimankar14@gmail.com>
2025-10-22 23:39:17 +0530

    Create transpose_matrix.c

---
+++ Function/Medium/transpose_matrix.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void transpose(int r,int c,int a[r][c],int t[c][r]){
+    for(int i=0;i<r;i++)
+        for(int j=0;j<c;j++)
+            t[j][i] = a[i][j];
+}
+int main(){
+    int r,c; scanf("%d %d",&r,&c);
+    int a[r][c], t[c][r];
+    for(int i=0;i<r;i++) for(int j=0;j<c;j++) scanf("%d",&a[i][j]);
+    transpose(r,c,a,t);
+    printf("Transpose matrix:\n");
+    for(int i=0;i<c;i++){ for(int j=0;j<r;j++) printf("%d ",t[i][j]); printf("\n"); }
+    return 0;
+}
