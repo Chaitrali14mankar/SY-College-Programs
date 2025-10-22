3b9c21768441dd8fad2cd1b0eaf770ec1779eae9
Chaitrali14mankar <chaitralimankar14@gmail.com>
2025-10-23 00:04:33 +0530

    Create min_array.c

---
+++ Switch Case/Easy/min_array.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter array size: ");
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++) scanf("%d",&arr[i]);
+    int min=arr[0];
+    for(int i=1;i<n;i++) if(arr[i]<min) min=arr[i];
+    printf("Minimum=%d\n",min);
+    return 0;
+}
