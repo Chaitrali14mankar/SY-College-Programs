c8f05b126c31e41f167777c0b4b793f95de0bcaf
Chaitrali Mankar <chaitralimankar14@gmail.com>
2025-11-02 22:08:08 +0530

    Create Singly linked list using structure and pointer.c

---
+++ Pointer/Hard/Singly linked list using structure and pointer.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+void printList(struct Node *head) {
+    while (head) {
+        printf("%d -> ", head->data);
+        head = head->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    struct Node *head = NULL, *temp, *newNode;
+    for (int i = 1; i <= 3; i++) {
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = i * 10;
+        newNode->next = NULL;
+        if (head == NULL)
+            head = temp = newNode;
+        else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+    printList(head);
+    return 0;
+}
