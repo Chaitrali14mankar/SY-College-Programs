68fdff573da31fac25f1e6b9aee9a847c6b1bd26
Chaitrali Mankar <chaitralimankar14@gmail.com>
2025-11-02 21:56:40 +0530

    Create  Access structure members using pointer and arrow operator.c

---
+++ Pointer/Easy/Access structure members using pointer and arrow operator.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+struct Student {
+    int id;
+    char name[20];
+};
+
+int main() {
+    struct Student s1 = {101, "Amit"};
+    struct Student *ptr = &s1;
+    printf("ID: %d\n", ptr->id);
+    printf("Name: %s\n", ptr->name);
+    return 0;
+}
